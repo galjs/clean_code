faaa9c5e75843f94303782b305a92da074c2e101
galjs <62336611+galjs@users.noreply.github.com>
2020-03-19 11:07:37 +0200

    added junction calss

---
+++ clean_code/junction.py
@@ -0,0 +1,38 @@
+
+class Junction():
+    def __init__(self, up, down, left, right):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+
+    def __init__(self):
+        self.up = None
+        self.down = None
+        self.left = None
+        self.right = None
+
+    def get_up(self):
+        return self.up
+
+    def get_down(self):
+        return self.down
+
+    def get_left(self):
+        return self.left
+
+    def get_right(self):
+        return self.right
+
+    def set_up(self, up):
+        self.up = up
+
+    def set_down(self, down):
+        self.down = down
+
+    def set_left(self, left):
+        self.left = left
+
+    def set_right(self, right):
+        self.right = right
+
