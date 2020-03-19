0b333d20b713c96842ff92cdd82518de0c3de993
galjs <62336611+galjs@users.noreply.github.com>
2020-03-19 18:39:14 +0200

    algorithem finds the finish

--- clean_code/bfs_maze.py
+++ clean_code/bfs_maze.py
@@ -1,52 +1,3 @@
-class Node():
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-    def get_next(self):
-        return self.next
-
-    def get_value(self):
-        return self.value
-
-    def set_next(self, node):
-        self.next = node
-
-    def set_value(self, value):
-        self.value = value
-
-    def has_next(self):
-        if self.next is None:
-            return False
-        return True
-
-class Queue():
-    def __init__(self):
-        self.first = None
-        self.last = None
-
-    def insert(value):
-        node = Node(value)
-        if self.first is None:
-            self.first = node
-            self.last = node
-        else:
-            last.set_next(node)
-            last = last.get_next()
-
-    def remove(self):
-        value = self.first.get_value()
-        first = self.first.get_next()
-        if first is None:
-            last = None
-        return value
-
-    def head(self):
-        return self.first.get_value()
-
-    def is_empty(self):
-        return self.first is None
-
 
 
 class BFS_search():
@@ -54,7 +5,7 @@ class BFS_search():
         self.graph = graph
         self.start_junction = self.get_start_junction()    
 
-        self.find_best_route()
+        printlen(self.find_best_route())
 
 
     def get_start_junction(self):
@@ -62,24 +13,25 @@ class BFS_search():
         return self.graph.get_graph()[start.get_row()][start.get_column()]
 
     def find_best_route(self):
-        nodes = Queue()
-        nodes.insert(self.start_junction)
+        nodes = []
+        nodes.append(self.start_junction)
         self.start_junction.set_is_discovered(True)
-        while not nodes.is_empty():
-            self.start_junction = nodes.remove()
+        while not len(nodes) == 0:
+            self.start_junction = nodes[0]
+            del nodes[0]
 
-            neighbours = get_neighbours_list(self.start_junction)
+            neighbours = self.get_neighbours(self.start_junction)
             for neighbour in neighbours:
                 if neighbour.is_finish:
-                    nodes.insert(neighbour)
+                    nodes.append(neighbour)
                     return nodes
 
                 if not neighbour.is_discovered():
-                    nodes.insert(neighbour)
+                    nodes.append(neighbour)
                     neighbour.set_is_discovered(True)
             
     
-    def get_neighbours_list(current_junction):
+    def get_neighbours(self, current_junction):
         neighbours = []
 
         if current_junction.has_up():
--- clean_code/game.py
+++ clean_code/game.py
@@ -1,6 +1,7 @@
 from maze import Maze
 from position import Position
 from graph import Graph
+from bfs_maze import BFS_search
 
 def main():
     # 1 = wall. 0 = empty
@@ -11,9 +12,11 @@ def main():
 
     board = Maze(8, state)
     board_as_graph = Graph(Position(1, 1), Position(2, 6), board)
+    find = BFS_search(board_as_graph)
     
     print(board)
     print(board_as_graph)
+
 
 
 if __name__ == "__main__":
