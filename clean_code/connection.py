763d554cadadaf80f8994a92330d33672c119d7c
galjs <62336611+galjs@users.noreply.github.com>
2020-03-19 11:16:10 +0200

    added Connection class

---
+++ clean_code/connection.py
@@ -0,0 +1,10 @@
+
+class Connection():
+    def __init__(self, length):
+        self. length = length
+
+    def get_length(self):
+        return self.length
+
+    def set_length(self, length):
+        self.length = length
