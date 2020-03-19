4f2e3836b251a2c1b499b1402596da75cc7c97f5
galjs <62336611+galjs@users.noreply.github.com>
2020-03-19 14:22:55 +0200

    sorting things out

--- clean_code/junction.py
+++ clean_code/junction.py
@@ -13,29 +13,17 @@ class Junction():
         self.left = None
         self.right = None
 
-    def get_up_value(self):
-        return self.up.get_length()
+    def get_up(self):
+        return self.up
 
-    def get_down_value(self):
-        return self.down.get_length()
+    def get_down(self):
+        return self.down
 
-    def get_left_value(self):
-        return self.left.get_length()
+    def get_left(self):
+        return self.left
 
-    def get_right_value(self):
-        return self.right.get_length()
-
-    def get_up_node(self):
-        return self.up.get_next()
-
-    def get_down_node(self):
-        return self.down.get_next()
-
-    def get_left_node(self):
-        return self.left.get_next()
-
-    def get_right_node(self):
-        return self.right.get_next()
+    def get_right(self):
+        return self.right
 
     def set_up(self, up):
         self.up = up
@@ -50,14 +38,14 @@ class Junction():
         self.right = right
 
     def has_up(self):
-        return self.up.get_next() is not None
+        return self.up is not None
 
     def has_down(self):
-        return self.down.get_next() is not None
+        return self.down is not None
 
     def has_left(self):
-        return self.left.get_next() is not None
+        return self.left is not None
 
     def has_right(self):
-        return self.right.get_next() is not None
+        return self.right is not None
 
